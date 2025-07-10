40e087399edd2a55a32101673664195718068b71
austinmgray210 <Austin.m.gray210@gmail.com>
2025-07-10 15:49:52 -0500

    added user password reset

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # get "up" => "rails/health#show", as: :rails_health_check
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
@@ -11,5 +11,22 @@ Rails.application.routes.draw do
   
   # root "posts#index"
   root "main#index"
+  
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update"
+  
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+  
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+  
+  delete "logout", to: "sessions#destroy"
+  
+  get "password/reset", to: "password_resets#new"
+  post "password/reset", to: "password_resets#create"
+  get "password/reset/edit", to: "password_resets#edit"
+  patch "password/reset/edit", to: "password_resets#update"
+  
   get "about", to: "about#index"
 end
