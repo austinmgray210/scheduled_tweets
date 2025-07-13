303c4497b11e801df1da9265a0259165203e1a18
austinmgray210 <Austin.m.gray210@gmail.com>
2025-07-13 13:46:49 -0500

    Final commit, too many changes between rails 6 to rails 8, and changes from twitter API v1.1 to X API v2 to keep tack of without going down a rabbit hole to debug

--- app/models/twitter_account.rb
+++ app/models/twitter_account.rb
@@ -1,6 +1,26 @@
 class TwitterAccount < ApplicationRecord
+  # require "x"
   has_many :tweet
   belongs_to :user
 
   validates :username, uniqueness: true
+
+  def client
+    # x_credentials = {
+    #   api_key:             Rails.application.credentials.dig(:twitter, :api_key),
+    #   api_key_secret:      Rails.application.credentials.dig(:twitter, :api_secret),
+    #   access_token:        token,
+    #   access_token_secret: secret
+    # }
+    #
+    # X::Client.new(**x_credentials)
+
+
+    Twitter::REST::Client.new do |config|
+      config.consumer_key        = Rails.application.credentials.dig(:twitter, :api_key).to_s
+      config.consumer_secret     = Rails.application.credentials.dig(:twitter, :api_secret).to_s
+      config.access_token        = token.to_s
+      config.access_token_secret = secret.to_s
+    end
+  end
 end
